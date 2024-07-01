a91bbbae036bf2ddd6e3a8c94edb2b54df33f2fc
Zainaaz Hansa <zainaazhansa@gmail.com>
2024-07-01 05:13:14 +0200

    fixed containers

--- src/Grade8.js
+++ src/Grade8.js
@@ -9,20 +9,19 @@ function Grade8() {
           {/* NAVBAR DUPLICATES AND DOES NOT REPLACE */}
           <div class="navbar"><Link to="/">Grades</Link> &gt; Grade 8 </div>
         
-          <div>
+          <div class="container">
             <h2>Grade 8 Subjects</h2>
-            <ul>
-              <li>
-                <Link to="/8Geography">Geography</Link>
-              </li>
-              <li>Mathematics</li>
-              <li>English HL</li>
-              <li>Afrikaans</li>
-              <li>History</li>
-              <li>EMS</li>
-              <li>Technology</li>
+            
+              <a class="link"> <Link to="/8Geography">Geography</Link></a>
+              
+              <a class="link">Mathematics</a>
+              <a class="link">English HL</a>
+              <a class="link">Afrikaans FAL</a>
+              <a class="link">EMS</a>
+              <a class="link">History</a>
+              <a class="link">Technology</a>
               {/* Add links for other subjects as needed */}
-            </ul>
+           
           </div>
 
 
