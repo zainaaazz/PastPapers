41bcbd1296e926d7a802e1b8c8740cee65e17430
Zainaaz Hansa <zainaazhansa@gmail.com>
2024-07-01 04:26:56 +0200

    browser router check

--- src/8Geography.js
+++ src/8Geography.js
@@ -3,22 +3,30 @@ import myPDF from './papers/8P1.pdf';
 
 function Geography() {
   return (
+
+    
     <div>
-      <h2>Geography Past Papers</h2>
-      <ul>
-        <li>  
-          <a href={myPDF ? myPDF : '/papers/8P1.pdf'} target="_blank" rel="noreferrer">
-          Click here to view the PDF - June 2018 Memo
-        </a>
-        </li>
-      
-        <li>June 2019 QP</li>
-        <li>June 2019 Memo</li>
-        <li>Nov 2018 QP</li>
-        <li>Nov 2018 Memo</li>
-        <li>Nov 2019 Memo</li>
-      </ul>
+      <div class="navbar"><Link to="/">Grades</Link> &gt; <Link to="/">Grade8</Link> &gt; Geography </div>
+
+      <div>
+        <h2>Geography Past Papers</h2>
+        <ul>
+          <li>  
+            <a href={myPDF ? myPDF : '/papers/8P1.pdf'} target="_blank" rel="noreferrer">
+            Click here to view the PDF - June 2018 Memo
+          </a>
+          </li>
+        
+          <li>June 2019 QP</li>
+          <li>June 2019 Memo</li>
+          <li>Nov 2018 QP</li>
+          <li>Nov 2018 Memo</li>
+          <li>Nov 2019 Memo</li>
+        </ul> </div>
+        
     </div>
+
+    
   );
 }
 
