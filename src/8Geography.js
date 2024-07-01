c7d43e92e09223149c4489788b4f53449ccad377
Zainaaz Hansa <zainaazhansa@gmail.com>
2024-07-01 02:11:09 +0200

    Created 8Geography.js

---
+++ src/8Geography.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Geography() {
+  return (
+    <div>
+      <h2>Geography Past Papers</h2>
+      <ul>
+        <li>June 2018 Memo</li>
+        <li>June 2019 QP</li>
+        <li>June 2019 Memo</li>
+        <li>Nov 2018 QP</li>
+        <li>Nov 2018 Memo</li>
+        <li>Nov 2019 Memo</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Geography;
